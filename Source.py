f3ed79324dc01a638a91377bebd82023da051e89
Matthew6975 <Mattation6975@gmail.com>
2025-11-30 16:08:26 -0500

    My first commit. Learning the basics of Git

---
+++ Source.py
@@ -0,0 +1,7 @@
+#This is a wordle bot that helps you solve wordles by suggesting possible words based on your inputs.
+#The goal is to have it provide the most probable words based on the current information recieved from previous guesses.
+
+import math
+from Five_Letter_Words import FiveLetterWords as words
+
+
