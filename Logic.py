76daf9687c46ddade7d4b93b0b226c9548b6c75b
Matthew6975 <Mattation6975@gmail.com>
2025-11-30 21:56:18 -0500

    Basics of the logic are up and runnning. Next step is to build the GUI so I can figuer out how I need to process the user input.

--- Logic.py
+++ Logic.py
@@ -5,10 +5,10 @@ import math
 from Five_Letter_Words import FiveLetterWords as words
 
 unknown = "_"  #Represents an unknown letter in the pattern.
-#Each position can be either a known letter, an array of possible letters (yellows from other locations), or unknown (_).
+#The first position can be either a known letter or unknown(_), and the second is an array of possible letters (yellows from other locations).
 #Add third element to track known incorrect letters for that position?
-# pos1 = ["h", ["h"]]
-pos1 = [unknown, {"p", "s"}]
+pos1 = ["h", ["h"]]
+# pos1 = [unknown, {"p", "s"}]
 pos2 = [unknown, {"p", "s"}]
 pos3 = [unknown, {"p", "s"}]
 pos4 = [unknown, {"s"}]
@@ -19,53 +19,55 @@ known_letters = pos1[0] + pos2[0] + pos3[0] + pos4[0] + pos5[0]
 
 guessed_words = []
 
-def guess_func():
-        global guess 
-        guess = input("Enter your guess of 5 letters: ").lower().strip()
-        if len(guess) != 5 or not guess.isalpha() or guess not in words:
-                print("Invalid input. Please enter a 5 letter word from the list.")
-                guess_func()
-        guessed_words.append(guess)
+# def guess_func():
+#         global guess 
+#         guess = input("Enter your guess of 5 letters: ").lower().strip()
+#         if len(guess) != 5 or not guess.isalpha() or guess not in words:
+#                 print("Invalid input. Please enter a 5 letter word from the list.")
+#                 guess_func()
+#         guessed_words.append(guess)
 
-guess_func()
+# guess_func()
 
 possible_words = []
 greens_passed = []
 yellows_passed = []
-# print("Pattern:", pattern)
-# print("Known letters:", known_letters)
-# print("Guessed words:", guessed_words)
+duplicates_removed = [] #here for testing purposes. Can be removed once log is solidand tested.
+print("Pattern:", pattern) #here for testing purposes. Can be removed once log is solidand tested.
+print("Known letters:", known_letters) #here for testing purposes. Can be removed once log is solidand tested.
+print("Guessed words:", guessed_words) #here for testing purposes. Can be removed once log is solidand tested.
 
+#Main logic loop. Goes through each word in the list and checks against the pattern.
+guess = "helps"  #Here for testing purposes. Replace with input function later.
 if guess in words:
         for w in words:
                 print("Checking word:", w)
                 for p in range(5):
-                        if w[p] in pattern[p][1]:
+                        #Checks for yellows. This needs to expand and will get A LOT more complex as it grows. Not sure how to deal with duplicates yet.
+                        if w[p] in pattern[p][1] and w not in yellows_passed:
                                 yellows_passed.append(w)
-                        if pattern[p][0] != unknown and w[p] == pattern[p][0]:
+                        #Checks for greens. This may need to expand but will likely be very simple.
+                        if pattern[p][0] != unknown and w[p] == pattern[p][0] and w not in greens_passed:
                                 greens_passed.append(w)
+
+# Combines results and rempoves duplicates. duplocates_removed is for testing purposes and should be removed once logic is complete.
 if greens_passed or yellows_passed:
-        possible_words = list(set(greens_passed) & set(yellows_passed))
+        shorter = greens_passed if len(greens_passed) < len(yellows_passed) else yellows_passed
+        for word in shorter:
+                if word in greens_passed and word in yellows_passed:
+                        duplicates_removed.append(word)
+        possible_words = list(set(greens_passed) | set(yellows_passed))
 
 
 
 print("greens passed:", greens_passed)
+print(len(greens_passed))
+print("\n")
 print("yellows passed:", yellows_passed)
+print(len(yellows_passed))
+print("\n")
+print("duplicates removed:", duplicates_removed)
+print(len(duplicates_removed))
+print("\n")
 print("possible words", possible_words)
 print(len(possible_words))
-
-
-
-
-
-
-
-
-
-
-
-
-                        # print("Pos" + str(p+1) + ":" + str(pattern[p]), end="")
-                        # print("\n")
-                        # for i in range(2):
-                        #         print(pattern[p][i] + ",", end="")
